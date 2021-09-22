495fb104266b60c462e3d911424d14f02d4f9179
Joel Hans <joel.g.hans@gmail.com>
2021-09-22 12:04:20 -0700

    Trying something

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 })
 
 module.exports = withBundleAnalyzer({
+  target: 'serverless',
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
   eslint: {
